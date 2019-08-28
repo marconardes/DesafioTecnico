d60bb37924e8ca69ecbe01e93f05e49321c5931c
Marco Nardes <marconardes@gmail.com>
2019-08-28 16:46:28 -0300

    Update Configuration.java

--- src/main/java/com/nardes/Configuration.java
+++ src/main/java/com/nardes/Configuration.java
@@ -1,13 +1,13 @@
 package com.nardes;
 
 public class Configuration {
-	public static final String ACESSKEY = "GOOGLQ4ECIX7BEHLDN5K5TDS";
+	public static final String ACESSKEY = "";
 	
 	
-	public static final String KEY="1af4797c-d23c-4f6f-9d63-9d8496a13e14";
+	public static final String KEY="";
 	public static final String APIURL = "https://api.bitmovin.com/v1/";
-	public static String AWSAccessKeyId = "AKIAJQGGUZWFEXWWAC2Q";
-	public static String AWSSecretKey = "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";
+	public static String AWSAccessKeyId = "";
+	public static String AWSSecretKey = "";
 
 	
 	public Configuration() {
