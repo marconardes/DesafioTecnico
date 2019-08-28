4ab7326a2ec9e85b5eed07e1aa4d3fdf0fe4aaf3
marco <marco@marco-S400CA>
2019-08-28 01:25:03 -0300

    update to test

--- src/main/java/com/nardes/HelloAppEngine.java
+++ src/main/java/com/nardes/HelloAppEngine.java
@@ -7,15 +7,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
 @WebServlet(name="HelloAppEngine", value="/hello")
 public class HelloAppEngine extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
+	  
+	  Storage storage = StorageOptions.getDefaultInstance().getService();
+  	String bucketName = "nardes-test";
+  	Bucket bucket= storage.create(BucketInfo.of(bucketName));
+  	
 
     response.setContentType("text/plain");
-    response.getWriter().println("Hello App Engine - Flexible!");
+
+		Page<Blob> blobs = bucket.list();
+		for (Blob blob : blobs.iterateAll()) {
+		  // do something with the blob
+			System.out.println(blob.getName());
+			response.getWriter().println("Hello App Engine - Flexible!");
+		}
+    
   }
 
   public static String getInfo() {
